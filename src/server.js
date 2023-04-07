c521a5e4953d543095fa01916d983d24fa899e03
Valeria Silveira <ssilveira@nosis.com>
2023-04-07 13:08:22 -0300

    try to handle errors

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const productsRouter = require("./router/products.js");
 const blogRouter = require("./router/blog.js");
+const cartRouter = require("./router/cart.js");
 const config = require("../config.js");
+const errorHandler = require("./middlewares/errorHandler.js");
 
 const app = express();
 
@@ -11,6 +13,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/products", productsRouter);
 app.use("/api/blog", blogRouter);
 app.use("/api/cart", cartRouter);
+
+app.use((err, req, res, next) => {
+  if (err) {
+    res.status(500).send(`Something was wrong: ${err.message}`);
+  }
+});
 
 const PORT = config.PORT;
 app.listen(PORT, () => {
